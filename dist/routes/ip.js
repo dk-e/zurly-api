c1514ad6c58e80335b0dd73005b3b136d9d8d4d4
zurly <d@zurly.me>
2024-01-16 00:22:10 +0000

    recompile

--- dist/routes/ip.js
+++ dist/routes/ip.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get("/", (req, res) => {
-    const ip = req.socket.remoteAddress;
+    const ip = req.headers["x-real-ip"] || req.socket.remoteAddress;
     res.type("text/plain");
     res.send(ip);
 });
