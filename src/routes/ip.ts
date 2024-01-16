9c1a773efce68eac0356479d78e706836aee4fe3
zurly <d@zurly.me>
2024-01-16 00:19:18 +0000

    added headers to IP route

--- src/routes/ip.ts
+++ src/routes/ip.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const ip = req.socket.remoteAddress;
+  const ip = req.headers["x-real-ip"] || req.socket.remoteAddress;
   res.type("text/plain");
   res.send(ip);
 });
