cd790c362b2bab697a785a86c245b7962e64a7dd
zurly <d@zurly.me>
2024-01-26 23:00:52 +0000

    Add success message to ip/json

--- src/routes/ip/json.ts
+++ src/routes/ip/json.ts
@@ -3,7 +3,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const ip = req.headers["x-real-ip"] || req.socket.remoteAddress;
-  res.json({ ip });
+  res.json({
+    success: true,
+    ip,
+  });
 });
 
 export default router;
